80d59802568664be242ec69bb2785ea3bac85da6
Cesar <cesar.alcoholado@hotmail.com>
2025-05-28 17:22:30 -0300

    added checkout component & items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import { RootLayout } from "./layouts/RootLayout"
 import { CartContextProvider } from "./context/CartContext"
+import { Checkout } from "./components/Checkout"
 
-const router = createBrowserRouter(createRoutesFromElements(<Route path='/' element={<RootLayout/>}></Route>))
+const router = createBrowserRouter(createRoutesFromElements(
+<Route path='/' element={<RootLayout/>}>
+  <Route path='/checkout' element={<Checkout/>}/>
+</Route>))
 
 export const App = () => {
 
--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState, type Dispatch, type SetStateAction } from "react"
 export function useLocalStorage <T> (key:string, defaultValue:T): [T, Dispatch<SetStateAction<T>>] {
 
   const [value, setValue] = useState(()=> {
+    
     try {
       const storedItems = localStorage.getItem(key);
       return storedItems ? JSON.parse(storedItems) : defaultValue;
